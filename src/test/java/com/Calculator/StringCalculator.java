c741762b294675145cb27980e748c5e97e2a040d
Sohan <159467173+Sohanpatil07@users.noreply.github.com>
2024-11-19 12:10:57 +0530

    added test cases for newlinedelimiter,customdelimlator and check negative numbers

--- src/test/java/com/Calculator/StringCalculator.java
+++ src/test/java/com/Calculator/StringCalculator.java
@@ -30,5 +30,24 @@ public class StringCalculator {
         assertEquals(15, calculatorService.add("5,10"));
     }
 
+    @Test
+    void testAddWithNewLineDelimiter() throws NegativeNumberException {
+        assertEquals(6, calculatorService.add("1\n2,3"));
+    }
+
+    @Test
+    void testAddWithCustomDelimiter() throws NegativeNumberException {
+        assertEquals(3, calculatorService.add("//;\n1;2"));
+    }
+
+    @Test
+    void testAddWithNegativeNumbers() {
+        NegativeNumberException exception = assertThrows(
+                NegativeNumberException.class,
+                () -> calculatorService.add("1,-2,-3")
+        );
+        assertEquals("Negative numbers not allowed: [-2, -3]", exception.getMessage());
+    }
+
 
 }
