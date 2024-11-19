860e485984ab9cc60402bdf6a574e2de963c4b84
Sohan <159467173+Sohanpatil07@users.noreply.github.com>
2024-11-19 10:52:55 +0530

    project setup

---
+++ src/main/java/com/Calculator/StringCalculatorTddProjectApplication.java
@@ -0,0 +1,13 @@
+package com.Calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringCalculatorTddProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringCalculatorTddProjectApplication.class, args);
+	}
+
+}
