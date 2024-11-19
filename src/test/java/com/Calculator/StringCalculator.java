fe2588a3eccef9cb7f0d516fff00507bae9fd0ac
Sohan <159467173+Sohanpatil07@users.noreply.github.com>
2024-11-19 12:04:06 +0530

    Add unit tests for empty string in CalculatorService

--- src/test/java/com/Calculator/StringCalculator.java
+++ src/test/java/com/Calculator/StringCalculator.java
@@ -1,11 +1,24 @@
 package com.Calculator;
 
+import com.Calculator.exception.NegativeNumberException;
+import com.Calculator.service.CalculatorService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 public class StringCalculator {
+
+    private final CalculatorService calculatorService = new CalculatorService();
+
+
+
+    @Test
+    void testAddWithEmptyString() throws NegativeNumberException {
+        assertEquals(0, calculatorService.add(""));
+    }
+
 
 }
